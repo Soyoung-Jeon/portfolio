99c424ded731d394f4889dc1c10ccf3fce9b33f8
전소영 <jsoy0423@gmail.com>
2020-02-19 17:48:47 +0900

    탭 기능 구현

--- js/index.js
+++ js/index.js
@@ -1,80 +1,75 @@
-// 데이터 객체 가져오기
-const itemImage = document.querySelector(".item_img");
-const itemType = document.querySelector(".item_type");
-const itemTitle = document.querySelector(".item_title");
-const itemContents = document.querySelector(".item_contents");
-const itemRefer = document.querySelector(".item_refer");
+class Gallery {
+  display() {
+    // 아이템 복사에 필요
+    const itemList = document.querySelector(".list_inner");
+    const itembox_origin = document.querySelector(".item_btn");
 
-let result = [];
+    let result = [];
 
-// 아이템 복사에 필요
-const itemList = document.querySelector(".list_inner");
-const itembox_origin = document.querySelector(".item_btn");
-itembox_origin.parentNode.removeChild(itembox_origin);
+    // 아이템 복사, 데이터 넣기
+    for (let i = 0; i < data.length; i++) {
+      const itembox = itembox_origin.cloneNode(true);
+      itembox.classList.add(this._typeToClassType(data[i].type));
 
-// 아이템 복사, 데이터 넣기
-for (let i = 0; i < data.length; i++) {
+      let itemImage = itembox.querySelector(".item_img > img");
+      let itemType = itembox.querySelector(".item_type");
+      let itemTitle = itembox.querySelector(".item_title");
+      let itemContents = itembox.querySelector(".item_contents");
+      let itemRefer = itembox.querySelector(".item_refer");
 
-    const itembox = itembox_origin.cloneNode(true);
-    itemList.appendChild(itembox);
+      itemImage.setAttribute("src", data[i].src);
+      itemType.innerText = data[i].type;
+      itemTitle.innerText = data[i].title;
+      itemContents.innerText = data[i].contents;
+      itemRefer.innerText = data[i].refer;
 
-    console.log(itembox)
+      result.push(itembox);
+      itemList.appendChild(itembox);
+    }
 
-    itemType.innerText = data[i].type;
-    itemTitle.innerText = data[i].title;
-    itemContents.innerText = data[i].contents;
-    itemRefer.innerText = data[i].refer;
-    
-    result.push(itembox);
+    addEventListener("click", event => {
+      this._tabSelected(event);
+    });
+  }
+  _typeToClassType(type) {
+    let result = "";
+    if (type == "웹사이트") {
+      result = "web";
+    } else if (type == "특집기사") {
+      result = "news";
+    } else {
+      result = "etc";
+    }
+    return result;
+  }
+
+  _setElementsDisplay(elements, value) {
+    for (let k = 0; k < elements.length; k++) {
+      elements[k].style.display = value;
+    }
+  }
+
+  _tabSelected(event) {
+    let itembox_all = document.querySelectorAll(".item_btn");
+
+    if (event.target.id == "") {
+      return;
+    }
+    if (event.target.id == "all") {
+      this._setElementsDisplay(itembox_all, "block");
+    } else {
+      let selected_elements = document.querySelectorAll(
+        ".item_btn." + event.target.id
+      );
+      this._setElementsDisplay(itembox_all, "none");
+      this._setElementsDisplay(selected_elements, "block");
+    }
+
+    document.querySelectorAll(".tab_btn").forEach(el => {
+        el.style.fontWeight = 300;
+    });
+    event.target.style.fontWeight = "900";
+  }
 }
 
-
-
-
-
-    const tab = document.querySelectorAll(".tab_inner > li > a");
-    const filter = data.filter(function(item){    
-        return item.type === "웹사이트";
-    }); 
-
-    
-    
-    for (let j = 0; j < 4; j++) {
-        tab[j].addEventListener('click', tabSelected);
-    }
-
-    function tabSelected(event){
-
-        const itembox_all = document.querySelectorAll(".item_btn");
-        const allhide = () => {
-            for (let k = 0; k < itembox_all.length; k++) {
-                itembox_all[k].style.display = "none";
-            }   
-        }
-        switch(event.target.id){
-            
-            case 'all':
-                for (let k = 0; k < itembox_all.length; k++) {
-                    itembox_all[k].style.display = "block";
-                }  
-                break;
-            case 'web':
-                allhide();
-                
-
-                for (let m = 0; m < filter.length; m++) {
-                    filter[m].style.display = "block";
-                } 
-
-                break;
-            case 'news':
-                allhide();
-                break;
-            case 'toy':
-                allhide();
-                break;
-        }
-    }
-    
-    
-
+new Gallery().display();
