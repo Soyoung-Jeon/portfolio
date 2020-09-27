1f1813f614572f45a82e45faf9f58c4847895666
soyoung <jsoy0423@gmail.com>
2020-09-27 17:42:40 +0900

    주소변경

--- js/data.js
+++ js/data.js
@@ -1,90 +1,90 @@
 const data = [
     {
-      href: "../pdf/Portfolio_01.pdf",
+      href: "pdf/Portfolio_01.pdf",
       src: "img/01.png",
       type: "프로젝트",
       title: "오라클 데이터베이스 서버 운영",
       contents: "DB 액세스 관리, 공간 관리, 유저 보안 관리"
     },
     {
-      href: "../pdf/Portfolio_02.pdf",
+      href: "pdf/Portfolio_02.pdf",
       src: "img/02.png",
       type: "프로젝트",
       title: "Backup & Recovery",
       contents: "DB 불완전 복구 수행"
 	},
 	{
-		href: "../pdf/Portfolio_03.pdf",
+		href: "pdf/Portfolio_03.pdf",
 		src: "img/03.png",
 		type: "프로젝트",
 		title: "DataPump를 활용한 Data Migration ",
 		contents: "기존 데이터를 새 데이터베이스로 이관"
 	},
 	{
-		href: "../pdf/RAC.pdf",
+		href: "pdf/RAC.pdf",
 		src: "img/08.png",
 		type: "매뉴얼",
 		title: "RAC설치",
 		contents: "RAC 구축하기"
 	},
 	{
-		href: "../pdf/ASM.pdf",
+		href: "pdf/ASM.pdf",
 		src: "img/10.png",
 		type: "매뉴얼",
 		title: "ASM",
 		contents: "Oracle 11g R2에서 ASM 사용하기"
 	},
 	{
-		href: "../pdf/LVM.pdf",
+		href: "pdf/LVM.pdf",
 		src: "img/09.png",
 		type: "매뉴얼",
 		title: "LVM",
 		contents: "LVM (Logical Volume Manager)"
 	},
 	{
-		href: "../pdf/manual_11g.pdf",
+		href: "pdf/manual_11g.pdf",
 		src: "img/04.png",
 		type: "매뉴얼",
 		title: "Oracle 11g R2 설치",
 		contents: "오라클 데이터베이스 11g 설치"
 	},
 	{
-		href: "../pdf/manual_12c.pdf",
+		href: "pdf/manual_12c.pdf",
 		src: "img/05.png",
 		type: "매뉴얼",
 		title: "Oracle 12c 설치",
 		contents: "오라클 데이터베이스 12c 설치"
 	},
 	{
-		href: "../pdf/manual_create.pdf",
+		href: "pdf/manual_create.pdf",
 		src: "img/06.png",
 		type: "매뉴얼",
 		title: "데이터베이스의 수동관리",
 		contents: "데이터베이스 수동삭제 및 수동생성"
 	},
 	{
-		href: "../pdf/manual_remote.pdf",
+		href: "pdf/manual_remote.pdf",
 		src: "img/07.png",
 		type: "매뉴얼",
 		title: "클라이언트/서버 접속 환경구축",
 		contents: "Oracle Net을 통한 원격접속"
 	},
 	{
-		href: "../pdf/RMAN.pdf",
+		href: "pdf/RMAN.pdf",
 		src: "img/11.png",
 		type: "매뉴얼",
 		title: "RMAN",
 		contents: "오라클 RMAN (Recovery Manager)"
 	},
 	{
-		href: "../pdf/TEMP_DB.pdf",
+		href: "pdf/TEMP_DB.pdf",
 		src: "img/12.png",
 		type: "매뉴얼",
 		title: "Temporary DB",
 		contents: "임시 데이터베이스를 통한 복구"
 	},
 	{
-		href: "../pdf/BR_controlfiles.pdf",
+		href: "pdf/BR_controlfiles.pdf",
 		src: "img/13.png",
 		type: "매뉴얼",
 		title: "컨트롤 파일 손상 시나리오",
